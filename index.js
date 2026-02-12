2b46afcfc8f3d502f70e78fe4fbad72bb551d4de
anveshvanam <anveshvanam5@gmail.com>
2026-02-12 13:44:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ app.use(
       "https://your-frontend-domain.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
   })
 );
+app.options("*", cors());
+
 
 app.use(express.json());
 app.use("/admin", adminRoutes);
